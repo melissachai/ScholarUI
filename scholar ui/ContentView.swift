70b327f1b0b26f4ae9c3452c628780b756189f07
Melissa Chai <melissachai40@gmail.com>
2023-06-26 10:29:42 -0700

    Initial Commit

---
+++ scholar ui/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  scholar ui
+//
+//  Created by Melissa Chai on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack{
+            Text("What is your name?")
+            TextField("", text: $name)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.gray, width: 1)
+            Button("Submit Name") {
+            textTitle = "Welcome"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+        }
+    }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scholar ui/scholar_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  scholar_uiApp.swift
+//  scholar ui
+//
+//  Created by Melissa Chai on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct scholar_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
